6c8da6a15ffa6b3277133ecac1b32139bb8b1a6f
Diogo Soares <dioggo.soares@live.com>
2023-07-06 01:28:25 -0300

    feat: Criando o Skeleton component

--- src/app/(auth)/sign-in/[[...sign-in]]/loading.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/loading.tsx
@@ -1,7 +1,9 @@
+import { Skeleton } from '@/components/ui/feedback/Skeleton'
+
 export default function Loading() {
   return (
     <div className="flex w-full flex-1 flex-col items-center justify-center p-6">
-      <p>Carregando...</p>
+      <Skeleton className="h-[30px] w-[334px] rounded-md bg-zinc-300" />
     </div>
   )
 }
