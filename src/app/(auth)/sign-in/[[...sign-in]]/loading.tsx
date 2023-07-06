da6a61e3205d78871fbb4419a45d1a0c88fb284f
Diogo Soares <dioggo.soares@live.com>
2023-07-06 02:01:15 -0300

    feat: Ajuste pontual no Skeleton

--- src/app/(auth)/sign-in/[[...sign-in]]/loading.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '@/components/ui/feedback/Skeleton'
 export default function Loading() {
   return (
     <div className="flex w-full flex-1 flex-col items-center justify-center p-6">
-      <Skeleton className="h-[30px] w-[334px] rounded-md bg-zinc-300" />
+      <Skeleton className="h-[1.875rem] w-[20.875rem] rounded-md bg-zinc-300" />
     </div>
   )
 }
